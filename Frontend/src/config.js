c50e51906c20f246396da423af998f8ff0160d1c
mugilsanjay90-wq <mugilsanjay90@gmail.com>
2026-09-24 21:14:02 +0530

    Update config.js

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,3 +1,3 @@
 // API URL Configuration
 // Change this to your Railway/Render backend URL in production
-export const API_URL = import.meta.env.VITE_API_URL || "https://smart-bus-backend-rho.vercel.app/";
+export const API_URL = import.meta.env.VITE_API_URL || "https://smart-bus-backend-rho.vercel.app";
